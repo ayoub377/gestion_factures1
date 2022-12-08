a95027c65d75e61a76cdaa9a2a07da0a88c6df22
ayoub377 <ayoubennaoui20@gmail.com>
2022-12-08 15:29:35 +0100

    first

--- gestion_factures/gateway/src/main/java/org/sid/gateway/GatewayApplication.java
+++ gestion_factures/gateway/src/main/java/org/sid/gateway/GatewayApplication.java
@@ -15,16 +15,13 @@ public class GatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
     }
-    //@Bean
+
+    @Bean
     public RouteLocator routeLocator(RouteLocatorBuilder builder){
         return builder.routes()
                 .route(r->r.path("/customers/**").uri("lb://CUSTOMER-SERVICE"))
                 .route(r->r.path("/products/**").uri("lb://INVENTORY-SERVICE"))
                 .build();
-    }
-    @Bean
-    public DiscoveryClientRouteDefinitionLocator dynamicRoutes(ReactiveDiscoveryClient rdc, DiscoveryLocatorProperties dlp){
-        return new DiscoveryClientRouteDefinitionLocator(rdc,dlp);
     }
 
 }
